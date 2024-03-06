c61d1ed63340674255a71abcb5a34054f39fa664
vaidish227 <vaidishsavar221@gmail.com>
2024-03-06 22:52:14 +0530

    Templates created

---
+++ icoder/home/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request,'home/home.html')
+    #return HttpResponse('this is home')
+
+def about(request):
+   return render(request,'home/about.html') 
+
+def contact(request):
+    return render(request,'home/contact.html')
+
